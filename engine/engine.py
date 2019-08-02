2f8ca1de8b18e70b79e05d6a476ceba2785e15c3
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-08-02 15:48:03 +0500

    Preps in engine for game first loading

--- engine/engine.py
+++ engine/engine.py
@@ -1,124 +1,59 @@
-from dataclasses import dataclass
+from maps.maps_processor import GameMap
+from gui.gui import GameGUI
+
+from time import (
+    time as current_time_in_seconds,
+    sleep as time_sleep)
+from threading import Thread
 
 
-@dataclass
-class Vector2D:
-    """Vector of two coordinates: X, Y.
+class GameEngine:
+    """All game logic processes here"""
 
-    All positions or position modifiers in game should be vectors of two coordinates
-    """
-    x: int
-    y: int
+    def __init__(self, input_game_map: GameMap):
+        self._game_map = input_game_map
 
-    def __add__(self, other):
-        return Vector2D(self.x + other.x, self.y + other.y)
+    _game_map: GameMap
+    _gui: GameGUI = GameGUI()
 
+    class MapUpdater:
+        """All map state updating logic is here"""
 
-class GameEngine:  # pragma: no cover # temp pragma
-    """Class realize game engine that moves movable game objects"""
-    pass
-#
-#     def __init__(self, game_map):  # pragma: no cover
-#         self.collisions_proc = CollisionsProcessor(game_map)
-#         self.moving_objects = []
-#         for game_object in game_map:
-#             if game_object.movable:
-#                 self.moving_objects += [game_object]
-#
-#         self.console_interface = ConsoleUserInterface(game_map)
-#
-#     def _catch_key(self):  # pragma: no cover
-#         '''Method awaits hitting on keyboard in endless loop and when key is \
-# hitted then that key is returned'''
-#         if platform_system() == 'Linux':
-#             return self._console_window.getch()
-#         elif platform_system() == 'Windows':
-#             if msvcrt_kbhit():
-#                 return msvcrt_getch() + msvcrt_getch()
-#         else:
-#             raise OSError("Operational system is not supported for program!")
-#
-#     def _interpret_catched_key(self, catched_key):  # pragma: no cover
-#         '''Method interprets catched key to direction or command \
-# to main game loop'''
-#         if catched_key == b'\xe0M' or catched_key == curses_KEY_RIGHT:
-#             return MoveDirection.Right
-#         elif catched_key == b'\xe0P' or catched_key == curses_KEY_DOWN:
-#             return MoveDirection.Down
-#         elif catched_key == b'\xe0K' or catched_key == curses_KEY_LEFT:
-#             return MoveDirection.Left
-#         elif catched_key == b'\xe0H' or catched_key == curses_KEY_UP:
-#             return MoveDirection.Up
-#         elif catched_key == b'q\x00' or catched_key == 113:
-#             return 'break'
-#         else:
-#             return 'continue'
-#
-#     def get_x_y_modifiers(move_direction):
-#         """Function return x and y modifiers out of given move direction"""
-#         x_modifier = 0
-#         y_modifier = 0
-#         if move_direction == MoveDirection.Right:
-#             x_modifier = 1
-#         elif move_direction == MoveDirection.Down:
-#             y_modifier = 1
-#         elif move_direction == MoveDirection.Left:
-#             x_modifier = -1
-#         elif move_direction == MoveDirection.Up:
-#             y_modifier = -1
-#         else:
-#             raise ValueError("Helper function got not a direction. "
-#                              "Gotten: "
-#                              + str(move_direction))
-#         return x_modifier, y_modifier
-#
-#     def start_game(self):  # pragma: no cover
-#         '''Main method that realizes main game loop'''
-#         if platform_system() == 'Linux':
-#             self._console_window = curses_initscr()
-#             self._console_window.keypad(True)
-#             self._console_window.refresh()
-#             curses_noecho()
-#             curses_cbreak()
-#         elif platform_system() == 'Windows':
-#             global \
-#                 curses_KEY_LEFT, \
-#                 curses_KEY_RIGHT, \
-#                 curses_KEY_DOWN, \
-#                 curses_KEY_UP
-#
-#             curses_KEY_RIGHT = -1
-#             curses_KEY_DOWN = -1
-#             curses_KEY_LEFT = -1
-#             curses_KEY_UP = -1
-#
-#         self.console_interface.draw_map()
-#         while True:
-#             interpreted_key = self._interpret_catched_key(self._catch_key())
-#             if interpreted_key not in MoveDirection:
-#                 if interpreted_key == 'continue':
-#                     continue
-#                 elif interpreted_key == 'break':
-#                     break
-#                 else:
-#                     raise ValueError('Key interpreted incorrectly!')
-#
-#             move_direction = interpreted_key
-#
-#             x_modifier, y_modifier = get_x_y_modifiers(move_direction)
-#             if self.collisions_proc.object_on_position_moved(
-#                self.moving_objects[0].current_position,
-#                x_modifier, y_modifier):
-#                 self.console_interface.move_object_on_position(
-#                     self.moving_objects[0].current_position,
-#                     x_modifier, y_modifier)
-#
-#                 obj_position = self.moving_objects[0].current_position
-#                 self.moving_objects[0].current_position = \
-#                     (obj_position[0] + x_modifier,
-#                      obj_position[1] + y_modifier)
-#
-#             self.console_interface.draw_map()
-#
-#         self.console_interface.clear_console()
-#         exit(0)
+        def update_map(self):
+            """Main method that should be invoked from game loop"""
+            # TODO
+            pass
+
+    def start_game(self):
+        """Initialize game loop"""
+        self._gui.init(self._game_map)
+
+        Thread(self._game_loop()).start()
+        # Right here several renderings might be lost. Not so critical for debugging
+        self._gui.run_gui_loop()
+
+    @staticmethod
+    def _time_alignment():
+        """Time alignment for CPU power saving
+
+        Игра работает в режиме 60 итераций игрового цикла (обновление И рендер уровня в
+        одной итерации) в секунду.
+
+        По сути, секунда разбита на 60 частей. Выравнивание происходит таким образом,
+        что в начале каждой 1\60 части секунды должна начинаться КАЖДАЯ итерация
+        игрового цикла. НЕТ гарантии, что при таком подходе не будет потеряна одна из
+        1\60-ой частей секунды
+
+        Таким образом, каждое обновление уровня происходит с рассчетом ТОЛЬКО на
+        текущую 1/60 часть секунды. Это позволяет избавиться от дробных величин при
+        модификации позиции движущихся объектов.
+        """
+        # All time below in milliseconds
+        one_iteration_time: int = 1000 // 60
+        millis_in_current_second: int = int(current_time_in_seconds() * 1000) % 1000
+        time_sleep(one_iteration_time - millis_in_current_second % one_iteration_time)
+
+    def _game_loop(self):
+        """Game loop has 3 parts in order: update, render, time alignment"""
+        # TODO
+        pass
