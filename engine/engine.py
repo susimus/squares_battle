914eb43552f7195766fd530ae8be8760f31824b5
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-08-02 19:04:37 +0500

    EventListener part realisation in engine

--- engine/engine.py
+++ engine/engine.py
@@ -1,18 +1,42 @@
 from maps.maps_processor import GameMap
-from gui.gui import GameGUI
+from gui.gui import GameGUI, EventListener
 
 from time import (
     time as current_time_in_seconds,
     sleep as time_sleep)
 from threading import Thread
+from typing import Set
 
 
-class GameEngine:
+class GameEngine(EventListener):
     """All game logic processes here"""
 
     def __init__(self, input_game_map: GameMap):
         self._game_map = input_game_map
         self._map_updater = self.MapUpdater(input_game_map)
+
+#
+# Event listener methods
+#
+
+    # All key codes of keys that are currently pressed. Without lock because modifying
+    # appears only in 'event listener' methods
+    _keysPressed: Set[int] = {}
+
+    def key_released(self, key_code: int):
+        self._keysPressed.remove(key_code)
+
+    def key_pressed(self, key_code: int):
+        self._keysPressed.add(key_code)
+
+    _game_closed: bool = False
+
+    def window_closed(self):
+        pass
+
+#
+# Main game loop section
+#
 
     class MapUpdater:
         """All map state updating logic is here"""
@@ -33,7 +57,7 @@ class GameEngine:
 
     def start_game(self):
         """Initialize game loop"""
-        self._gui.init(self._game_map)
+        self._gui.init(self._game_map, self)
 
         Thread(target=self._game_loop).start()
         # Right here several renderings might be lost. Not so critical for debugging
@@ -58,15 +82,16 @@ class GameEngine:
         # All time below in milliseconds
         one_iteration_time: int = 1000 // 60
         millis_in_current_second: int = int(current_time_in_seconds() * 1000) % 1000
-        time_sleep(one_iteration_time - millis_in_current_second % one_iteration_time)
+        time_sleep(
+            (one_iteration_time - millis_in_current_second % one_iteration_time) / 1000)
 
     def _game_loop(self):
         """Game loop has 3 parts in order: update, render, time alignment"""
-        # TODO
-        while True:
+        while True:  # TODO: Change condition to 'game_is_not_closed'
             self._map_updater.update_map()
-
+            # print(self._keysPressed)
             # Check here if game is not closed
+            # self._gui.destroy()
             self._gui.render()
 
             self._time_alignment()
