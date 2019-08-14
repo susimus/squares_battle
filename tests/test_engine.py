5fdb919403b123ba69bc1965427d75156ae3e2c6
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-08-14 13:30:25 +0500

    Add engine tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,10 +1,40 @@
-import unittest
+from engine.engine import GameEngine
+from maps.maps_processor import GameMap
+
+from unittest import TestCase
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class VelocityGettersTests(TestCase):
+    _state_updater: GameEngine.MapUpdater.StateUpdater = (
+        GameEngine.MapUpdater.StateUpdater(GameMap(), set()))
 
+    def test_velocity_to_the_left(self):
+        assert self._state_updater._get_horizontal_velocity(
+            {self._state_updater._KEY_CODE_A}) == -self._state_updater._PLAYER_MOVE_SPEED
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_velocity_to_the_right(self):
+        assert self._state_updater._get_horizontal_velocity(
+            {self._state_updater._KEY_CODE_D}) == self._state_updater._PLAYER_MOVE_SPEED
+
+    def test_no_horizontal_velocity_with_both_keys_pressed(self):
+        assert self._state_updater._get_horizontal_velocity(
+            {self._state_updater._KEY_CODE_A, self._state_updater._KEY_CODE_D}) == 0
+
+    def test_no_horizontal_velocity_with_no_keys_pressed(self):
+        assert self._state_updater._get_horizontal_velocity(set()) == 0
+
+    def test_initial_jump_velocity(self):
+        self._state_updater._jump_is_available = True
+
+        assert self._state_updater._get_vertical_velocity(
+            {self._state_updater._KEY_CODE_SPACE}) == (
+                self._state_updater._INITIAL_JUMP_VELOCITY)
+
+    def test_gravity_acceleration(self):
+        assert self._state_updater._get_vertical_velocity(set()) == (
+            self._state_updater._GRAVITY_ACCELERATION.y)
+
+    def test_max_vertical_velocity(self):
+        self._state_updater._vertical_velocity = 100
+
+        assert self._state_updater._get_vertical_velocity(set()) == 12
