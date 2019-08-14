9482e943310f3a59ebb8e72e8e83880c7a69f63d
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-08-14 17:58:58 +0500

    Add python version check

--- launcher.py
+++ launcher.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser
+from sys import version_info as sys_version_info, exit as sys_exit
 
 from engine.engine import GameEngine
 from maps.maps_processor import RawMapsContainer
@@ -8,6 +9,10 @@ GAME_VERSION = "1"
 
 
 if __name__ == '__main__':  # pragma: no cover
+
+    if (sys_version_info.major < 3
+            or (sys_version_info.major == 3 and sys_version_info.minor < 7)):
+        sys_exit('Python version 3.7 or upper is required')
 
     parser = ArgumentParser(
         description='Videogame-platformer where squares fight for the win!')
