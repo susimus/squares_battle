e372f8046a644f16d687e6bcf741dc934fcf0680
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-10-18 17:16:26 +0500

    Refactoring
    * Move [GameEvent] and [Collision] below [CollisionsProcessor]
    * Remove [_narrow_check_switch]. Too difiicult for understanding
    * Add exception raising in case if object with unknown type is gotten

--- engine/collisions_processor.py
+++ engine/collisions_processor.py
@@ -1,78 +1,52 @@
-from typing import Dict, Callable, List, TypeVar, Optional
+from typing import List, Optional
 from enum import Enum
 from dataclasses import dataclass
 
 from maps.maps_processor import GameMap
 from engine.game_objects import (
     MovableObject, GameObject, Vector2D, PaintingConst, Player)
-
-
-class GameEvent(Enum):
-    """Enumerates all events that can occur in process of game object moving"""
-    PLAYER_IS_OUT_RIGHT = object()
-    PLAYER_IS_OUT_LEFT = object()
-    PLAYER_IS_OUT_TOP = object()
-    PLAYER_IS_OUT_BOTTOM = object()
-
-
-@dataclass
-class Collision:
-    moving_object: MovableObject
-    game_event: GameEvent
-
-    # May be 'None'. For example, if game object is out of game field's borders
-    collided_object: Optional[GameObject]
+from engine import ApplicationException
 
 
 class CollisionsProcessor:
     _game_map: GameMap
 
-    MovableObject = TypeVar('MovableObject', covariant=True)
-    NarrowPhaseFunc = Callable[
-        [MovableObject,  # Moving object
-         Vector2D,  # Moving vector
-         List[GameObject]  # Possibly collided objects
-         ],
-        List[Collision]]
-
-    _narrow_check_switch: Dict[str, NarrowPhaseFunc]
-
     def __init__(self, input_map: GameMap):
         self._game_map = input_map
-
-        # Add narrow check functions into switch here
-        self._narrow_check_switch = {
-            "Player": self._check_player_collisions}
 
     def get_collisions(
             self,
             moving_object: GameObject,
-            moving_vector: Vector2D) -> List[Collision]:
+            moving_vector: Vector2D) -> List['Collision']:
         """Main collisions acquiring method
 
-        If there is no collisions then empty list is returned"""
-
-        # TODO: 'Middle' phase where every two objects' circles collision is
-        #  checked. Every game object have its circle that contains this
-        #  object. It is needed to compare square of sum of two radiuses and
-        #  sum of squares of vector between centers of two circles. (Not root
-        #  in second case for more performance)
+        If there is no collisions then empty list is returned
+        """
+        # Optimization: 'Middle' phase where every two objects' circles
+        #  collision is checked. Every game object have its circle that
+        #  contains this object. It is needed to compare square of sum of
+        #  two radiuses and sum of squares of vector between centers of two
+        #  circles. (Not root in second case for more performance)
 
         # 'Narrow' phase with detailed check
-        narrow_phase_func = self._narrow_check_switch.get(
-            moving_object.__class__.__name__, None)
-        if narrow_phase_func is None:
-            raise ValueError(
-                'Got unknown class of [moving_object]: '
+
+        result_collisions: List[Collision]
+
+        if isinstance(moving_object, Player):
+            result_collisions = self._check_player_collisions(
+                moving_object, moving_vector)
+        else:
+            raise CollisionsProcessorException(
+                'While processing [get_collisions] method, '
+                'got [moving_object] with unknown type: '
                 + moving_object.__class__.__name__)
 
-        return narrow_phase_func(moving_object, moving_vector, [])
+        return result_collisions
 
     def _check_player_collisions(
             self,
             player: Player,
-            moving_vector: Vector2D,
-            possibly_collided_objects: List[GameObject]) -> List[Collision]:
+            moving_vector: Vector2D) -> List['Collision']:
         result_collisions: List[Collision] = []
 
         self._check_player_with_borders_collisions(
@@ -86,7 +60,7 @@ class CollisionsProcessor:
             self,
             player: Player,
             moving_vector: Vector2D,
-            result_collisions: List[Collision]):
+            result_collisions: List['Collision']):
         # Horizontal
         if (player.current_position.x + PaintingConst.PLAYER_SIDE_LENGTH
                 + moving_vector.x > self._game_map.game_field_size.x):
@@ -106,3 +80,24 @@ class CollisionsProcessor:
         elif player.current_position.y + moving_vector.y < 0:
             result_collisions.append(
                 Collision(player, GameEvent.PLAYER_IS_OUT_TOP, None))
+
+
+@dataclass
+class Collision:
+    moving_object: MovableObject
+    game_event: 'GameEvent'
+
+    # May be 'None'. For example, if game object is out of game field's borders
+    collided_object: Optional[GameObject]
+
+
+class GameEvent(Enum):
+    """Enumerates all events that can occur in process of game object moving"""
+    PLAYER_IS_OUT_RIGHT = object()
+    PLAYER_IS_OUT_LEFT = object()
+    PLAYER_IS_OUT_TOP = object()
+    PLAYER_IS_OUT_BOTTOM = object()
+
+
+class CollisionsProcessorException(ApplicationException):
+    pass
