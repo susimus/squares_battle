fc28e3c53ab7b0287cb2c2d681eff5dd49e08ba8
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-10-18 14:47:58 +0500

    Refactoring
    1. Now Vector2D have no default values
    2. Rename [fired_by_player] to [fired_player]
    3. Fix comment

--- engine/game_objects.py
+++ engine/game_objects.py
@@ -8,8 +8,8 @@ class Vector2D:
     All positions or position modifiers in game should be vectors of two
     coordinates
     """
-    x: float = 0
-    y: float = 0
+    x: float
+    y: float
 
     def __add__(self, other):
         return Vector2D(self.x + other.x, self.y + other.y)
@@ -66,19 +66,18 @@ class Player(MovableObject):
 @dataclass
 class BasicProjectile(MovableObject):
     """Small projectile-sphere that just flies forward with average speed"""
-    # Always fired by some Player. Number specifies Player instance
-    fired_by_player: int
+    fired_player: Player
 
 
 # TODO: Implement [InterfaceObject]
 class InterfaceObject(GameObject):
     """Abstraction for exact rendering order
 
-        Rendering order:
-        1. Interface objects
-        2. Immovable objects
-        3. Movable objects
-        """
+    Rendering order:
+    1. Interface objects
+    2. Immovable objects
+    3. Movable objects
+    """
     pass
 
 
