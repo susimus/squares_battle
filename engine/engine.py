2911ababf67014c2cedd1cd45e0084cf37f880ae
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-10-22 00:05:26 +0500

    Add buffs
    * Add global movement multipliers for buffs:
    [_PMS_gb_multiplier]
    [_IJV_gb_multiplier]
    * Add [_get_game_loop_iterations_count] reference method
    * Add [_refresh_global_movement_multipliers]
    * Add [_check_player_buffs]
    * Add [PLAYER_BUFF] collision event handling
    * Add [update_immovable_objects_states] method
    * Add [_update_buff_state] method
    * Move [_game_loop] method
    * Add [get_game_loop_iterations_count] geeter

--- engine/engine.py
+++ engine/engine.py
@@ -2,11 +2,17 @@ from time import (
     time as current_time_in_seconds,
     sleep as time_sleep)
 from threading import Thread
-from typing import Set, List
+from typing import Set, List, Callable
 
 from maps.maps_processor import GameMap
 from user_interface.game_ui import GameGUI, EventListener
-from engine.game_objects import *
+from engine.game_objects import (
+    Vector2D,
+    Player,
+    PaintingConst,
+    AbstractBuff,
+    SpeedUpBuff,
+    JumpHeightUpBuff)
 from engine.collisions_processor import (
     CollisionsProcessor, Collision, GameEvent)
 from engine import ApplicationException
@@ -22,20 +28,27 @@ class GameEngine(EventListener):
             _KEY_CODE_D: int = 68
             _KEY_CODE_SPACE: int = 32
 
+            # Constant move speed with global multiplier
             _PLAYER_MOVE_SPEED: int = 6
+            _PMS_gb_multiplier: float
 
             _GRAVITY_ACCELERATION: Vector2D = Vector2D(0, 2.5)
             _MAX_VERTICAL_VELOCITY: float = 12
+
+            # Constant initial jump velocity with global multiplier
             _INITIAL_JUMP_VELOCITY: float = -25
+            _IJV_gb_multiplier: float
 
             _collisions_processor: CollisionsProcessor
 
             _game_map: GameMap
             _keys_pressed: Set[int]
 
-            _jump_is_available: bool
+            _player_is_on_the_ground: bool
 
             _vertical_velocity: float
+
+            _get_game_loop_iterations_count: Callable[[], int]
 
             def __init__(self, game_engine: 'GameEngine'):
                 self._game_map = game_engine._game_map
@@ -43,22 +56,34 @@ class GameEngine(EventListener):
                 self._collisions_processor = CollisionsProcessor(
                     game_engine._game_map)
 
-                self._jump_is_available = False
+                self._player_is_on_the_ground = False
                 self._vertical_velocity = 0
 
+                self._PMS_gb_multiplier = 1
+                self._IJV_gb_multiplier = 1
+
+                self._get_game_loop_iterations_count = (
+                    game_engine.get_game_loop_iterations_count)
+
             def update_movable_objects_states(self):
+                self._refresh_global_movement_multipliers()
+
                 for movable_object in self._game_map.movable_objects:
                     if isinstance(movable_object, Player):
                         self._update_player_state(movable_object)
+
                     else:
                         GameEngineException(
                             "While processing [update_movable_objects_states] "
                             "method, got [movable_object] with unknown type: "
                             + movable_object.__class__.__name__)
 
-            # WouldBeBetter: Change vertical velocity to 0 when
-            #  PLAYER_IS_OUT_BOTTOM is gotten
+            def _refresh_global_movement_multipliers(self):
+                self._PMS_gb_multiplier = self._IJV_gb_multiplier = 1
+
             def _update_player_state(self, player: Player):  # pragma: no cover
+                self._check_player_buffs(player)
+
                 # Copy in case if '_keys_pressed' will be modified during check
                 keys_pressed_copy: Set[int] = set(self._keys_pressed)
                 player_move_vector: Vector2D = Vector2D(0, 0)
@@ -72,41 +97,93 @@ class GameEngine(EventListener):
                     player_collisions: List[Collision] = (
                         self._collisions_processor.get_collisions(
                             player, player_move_vector))
+
+                    # Optimization: Delegate this switch to a method?
                     for collision in player_collisions:
                         if (collision.game_event
-                                is GameEvent.PLAYER_IS_OUT_RIGHT):
+                                is GameEvent.PLAYER_BORDERS_RIGHT):
                             player_move_vector.x = 0
-                            player.current_position.x = (
+                            player.location.x = (
                                 self._game_map.game_field_size.x
                                 # '+ 1' for closest to border drawing
                                 - PaintingConst.PLAYER_SIDE_LENGTH + 1)
 
                         elif (collision.game_event
-                              is GameEvent.PLAYER_IS_OUT_LEFT):
+                              is GameEvent.PLAYER_BORDERS_LEFT):
                             player_move_vector.x = 0
-                            player.current_position.x = 0
+                            player.location.x = 0
 
                         elif (collision.game_event
-                              is GameEvent.PLAYER_IS_OUT_BOTTOM):
+                              is GameEvent.PLAYER_GROUND):
                             player_move_vector.y = 0
-                            player.current_position.y = (
-                                self._game_map.game_field_size.y
-                                # '+ 1' for closest to border drawing
-                                - PaintingConst.PLAYER_SIDE_LENGTH + 1)
 
-                            self._jump_is_available = True
+                            if collision.collided_object is None:
+                                # Teleport Player close to game borders
+                                player.location.y = (
+                                    self._game_map.game_field_size.y
+                                    - PaintingConst.PLAYER_SIDE_LENGTH + 1)
+
+                            # elif isinstance(
+                            #         collision.collided_object,
+                            #         BasicPlatform):
+                            #     player.location.y = (
+                            #         collision.collided_object.location.y
+                            #         - PaintingConst.PLAYER_SIDE_LENGTH)
+
+                            else:
+                                GameEngineException(
+                                    "[_update_player_state] method got "
+                                    "[collision] with [PLAYER_GROUND] as "
+                                    "[game_event] and unknown object as "
+                                    "[collided_object]: "
+                                    + collision
+                                    .collided_object.__class__.__name__)
+
+                            self._player_is_on_the_ground = True
 
                         elif (collision.game_event
-                              is GameEvent.PLAYER_IS_OUT_TOP):
+                              is GameEvent.PLAYER_BORDERS_TOP):
                             player_move_vector.y = 0
-                            player.current_position.y = 0
+                            player.location.y = 0
+
+                        elif (collision.game_event
+                              is GameEvent.PLAYER_BUFF):
+                            if not isinstance(
+                                    collision.collided_object, AbstractBuff):
+                                raise GameEngineException(
+                                    "[_update_player_state] got [PLAYER_BUFF]"
+                                    "as [game_event] and not [AbstractBuff]"
+                                    "instance as [collided_object]: "
+                                    + collision
+                                    .collided_object.__class__.__name__)
+
+                            collision.collided_object.capture_this_buff(
+                                self._get_game_loop_iterations_count(),
+                                player)
 
                         else:
-                            raise ValueError(
-                                "Got unknown [game_event]: "
+                            raise GameEngineException(
+                                "While processing [_update_player_state] "
+                                "method, got unknown [game_event]: "
                                 + collision.game_event.name)
 
-                    player.current_position += player_move_vector
+                    if player_move_vector.y != 0:
+                        self._player_is_on_the_ground = False
+
+                    player.location += player_move_vector
+
+            def _check_player_buffs(self, player: Player):
+                for buff in player.current_buffs:
+                    if isinstance(buff, SpeedUpBuff):
+                        self._PMS_gb_multiplier = 2
+
+                    elif isinstance(buff, JumpHeightUpBuff):
+                        self._IJV_gb_multiplier = 1.5
+
+                    else:
+                        raise GameEngineException(
+                            "[_check_player_buffs] method got [buff] with "
+                            "unknown type: " + buff.__class__.__name__)
 
             def _get_horizontal_velocity(
                     self, keys_pressed: Set[int]) -> float:
@@ -114,20 +191,28 @@ class GameEngine(EventListener):
                 input_move_vector: Vector2D = Vector2D(0, 0)
 
                 if self._KEY_CODE_A in keys_pressed:
-                    input_move_vector.x += -self._PLAYER_MOVE_SPEED
+                    input_move_vector.x += (
+                        -self._PLAYER_MOVE_SPEED * self._PMS_gb_multiplier)
 
                 if self._KEY_CODE_D in keys_pressed:
-                    input_move_vector.x += self._PLAYER_MOVE_SPEED
+                    input_move_vector.x += (
+                        self._PLAYER_MOVE_SPEED * self._PMS_gb_multiplier)
 
                 return input_move_vector.x
 
+            # TODO: Vertical velocity for specific player due to personal buffs
             def _get_vertical_velocity(self, keys_pressed: Set[int]) -> float:
                 """Method calculates current player's vertical velocity"""
                 if (
+                        self._KEY_CODE_SPACE not in keys_pressed
+                        and self._player_is_on_the_ground):
+                    self._vertical_velocity = self._GRAVITY_ACCELERATION.y
+                elif (
                         self._KEY_CODE_SPACE in keys_pressed
-                        and self._jump_is_available):
-                    self._vertical_velocity = self._INITIAL_JUMP_VELOCITY
-                    self._jump_is_available = False
+                        and self._player_is_on_the_ground):
+                    self._vertical_velocity = (
+                        self._INITIAL_JUMP_VELOCITY * self._IJV_gb_multiplier)
+                    self._player_is_on_the_ground = False
 
                 elif (self._vertical_velocity + self._GRAVITY_ACCELERATION.y
                       < self._MAX_VERTICAL_VELOCITY):
@@ -138,7 +223,22 @@ class GameEngine(EventListener):
 
                 return self._vertical_velocity
 
-        # TODO
+            def update_immovable_objects_states(self):
+                for immovable_object in self._game_map.immovable_objects:
+                    if isinstance(immovable_object, AbstractBuff):
+                        self._update_buff_state(immovable_object)
+
+                    else:
+                        GameEngineException(
+                            "[update_immovable_objects_states] method got "
+                            "[immovable_object] with unknown type: "
+                            + immovable_object.__class__.__name__)
+
+            def _update_buff_state(self, buff: AbstractBuff):
+                buff.check_buff_expiration(
+                    self._get_game_loop_iterations_count())
+
+        # TODO: Implement [GameObjectsSpawner]
         # class GameObjectsSpawner:
         #     """All game objects spawning is here"""
         #     _game_map: GameMap
@@ -155,8 +255,7 @@ class GameEngine(EventListener):
             All update methods are here"""
             # WouldBeBetter: Update interface objects states
 
-            # WouldBeBetter: Update immovable objects states. For example,
-            #  update state of immovable but rotating buff
+            self._state_updater.update_immovable_objects_states()
 
             self._state_updater.update_movable_objects_states()
 
@@ -209,6 +308,18 @@ class GameEngine(EventListener):
         # Right here several renderings CANNOT be lost
         self._gui.run_gui_loop()
 
+    def _game_loop(self):  # pragma: no cover
+        # Game loop is in a daemon thread so it will proceed until user
+        # interface thread is closed
+        while True:
+            self._map_updater.update_map()
+
+            self._gui.render()
+
+            self._time_alignment()
+
+            self._game_loop_iterations_count += 1
+
     @staticmethod
     def _time_alignment():  # pragma: no cover
         """Time alignment for CPU power saving
@@ -234,17 +345,8 @@ class GameEngine(EventListener):
              - millis_in_current_second % one_iteration_time)
             / 1000)
 
-    def _game_loop(self):  # pragma: no cover
-        # Game loop is in a daemon thread so it will proceed until user
-        # interface thread is closed
-        while True:
-            self._map_updater.update_map()
-
-            self._gui.render()
-
-            self._time_alignment()
-
-            self._game_loop_iterations_count += 1
+    def get_game_loop_iterations_count(self):
+        return self._game_loop_iterations_count
 
 
 class GameEngineException(ApplicationException):
