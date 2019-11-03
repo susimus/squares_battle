50fcac3ae764b06142f5d9c8bd0a48b5ac1851bb
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-11-03 19:03:23 +0500

    Move [GameMap], [RawMapsContainer] into 'maps'

---
+++ maps/__init__.py
@@ -0,0 +1,69 @@
+from copy import deepcopy
+from typing import Tuple
+
+from engine.game_objects import *
+from engine import ApplicationException
+
+
+DEFAULT_RESOLUTION: Tuple[int, int] = (1000, 700)
+
+
+class GameMap:
+    """
+
+    Rendering order:
+    1. Interface objects
+    2. Immovable objects
+    3. Movable objects
+    """
+    # Size coordinates must be non-negative. Immutable when game field is
+    # showing
+    game_field_size: Vector2D
+
+    # WouldBeBetter: Implement [interface_objects]
+    immovable_objects: List[ImmovableObject]
+    movable_objects: List[MovableObject]
+
+    def __init__(
+            self,
+            input_game_field_size: Vector2D,
+            input_immovable_objects: List[ImmovableObject],
+            input_movable_objects: List[MovableObject]):
+        if input_game_field_size.x < 0 or input_game_field_size.y < 0:
+            raise ApplicationException(
+                'Got negative game field size in process of game map init: '
+                + str(input_game_field_size))
+
+        self.game_field_size = deepcopy(input_game_field_size)
+        self.immovable_objects = deepcopy(input_immovable_objects)
+        self.movable_objects = deepcopy(input_movable_objects)
+
+
+class RawMapsContainer:
+    """Contains maps initializations via code"""
+    @staticmethod
+    def get_map_1() -> GameMap:
+        """One player only"""
+        return GameMap(
+            Vector2D(*DEFAULT_RESOLUTION),
+            [],
+            [Player(Vector2D(10, 10))])
+
+    @staticmethod
+    def get_map_2() -> GameMap:
+        """Player with single platform"""
+        return GameMap(
+            Vector2D(*DEFAULT_RESOLUTION),
+            [BasicPlatform(Vector2D(400, 600), 200, 30),
+             BasicPlatform(Vector2D(0, 600), 20, 100),
+             BasicPlatform(Vector2D(980, 600), 20, 100)],
+            [Player(Vector2D(101, 101))])
+
+    @staticmethod
+    def get_map_3() -> GameMap:
+        """Player with two buffs"""
+        return GameMap(
+            Vector2D(*DEFAULT_RESOLUTION),
+            [SpeedUpBuff(Vector2D(500, 550)),
+             JumpHeightUpBuff(Vector2D(1000, 550))],
+            [Player(Vector2D(0, 0))])
