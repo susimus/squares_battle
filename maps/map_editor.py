d47c341dc3e3b02007012bba53320c457a718f60
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-11-04 17:31:06 +0500

    Debug save button. Add load button

--- maps/map_editor.py
+++ maps/map_editor.py
@@ -10,7 +10,7 @@ from os.path import (
     dirname as os_path_dirname,
     abspath as os_path_abspath,
     pardir as os_pardir)
-from pickle import dump as pickle_dump
+from pickle import dump as pickle_dump, load as pickle_load
 
 try:
     from maps import GameMap, DEFAULT_RESOLUTION
@@ -38,16 +38,18 @@ class MapEditor(tk_Frame):
     _speed_up_buff_button: tk_Button
     _jump_height_up_buff_button: tk_Button
 
-    _filename_entry: tk_Entry
+    _save_filename_entry: tk_Entry
     _save_button: tk_Button
+
+    _load_filename_entry: tk_Entry
+    _load_button: tk_Button
 
     # TODO: Implement [init]
     def init(self):
         self.grid(row=0, column=0)
 
         self._current_game_map = GameMap(
-            # Debug: Player inside [movable_objects]
-            Vector2D(*DEFAULT_RESOLUTION), [], [Player(Vector2D(0, 0))])
+            Vector2D(*DEFAULT_RESOLUTION), [], [])
 
         self._init_slave_widgets()
 
@@ -55,13 +57,13 @@ class MapEditor(tk_Frame):
         self._game_gui = GameGUI(
             input_widgets_root=self.master, master=self)
         self._game_gui.init(self._current_game_map, EventListener())
-        self._game_gui.grid(row=0, column=0, rowspan=5)
-
-        self._game_gui.paint_all_game_objects()  # Debug
+        self._game_gui.grid(row=0, column=0, rowspan=7)
 
         self._init_creation_buttons()
 
         self._init_saving_widgets()
+
+        self._init_loading_widgets()
 
     def _init_creation_buttons(self):
         self._basic_platform_button = tk_Button(
@@ -113,23 +115,37 @@ class MapEditor(tk_Frame):
             if self._basic_platform_button['relief'] == 'sunken':
                 self._basic_platform_button.configure(relief='raised')
 
-    # TODO: Implement [_init_saving_widgets]
     def _init_saving_widgets(self):
-        self._filename_entry = tk_Entry(master=self)
-        self._filename_entry.grid(row=3, column=1)
+        self._save_filename_entry = tk_Entry(master=self)
+        self._save_filename_entry.grid(row=3, column=1)
 
         self._save_button = tk_Button(master=self, text='Save')
         self._save_button.grid(row=4, column=1)
         self._save_button.configure(command=self._save_button_command)
 
     def _save_button_command(self):
-        if self._filename_entry.get() != '':
-            with open(self._filename_entry.get(), 'wb') as file_handle:
+        if self._save_filename_entry.get() != '':
+            with open(self._save_filename_entry.get(), 'wb') as file_handle:
                 pickle_dump(self._current_game_map, file_handle)
 
-                self._current_game_map = GameMap(
-                    Vector2D(*DEFAULT_RESOLUTION), [], [])
-                self._game_gui.paint_all_game_objects()  # Debug: Not painting?
+                self._current_game_map.remove_all_game_objects()
+
+                self._game_gui.render()
+
+    def _init_loading_widgets(self):
+        self._load_filename_entry = tk_Entry(master=self)
+        self._load_filename_entry.grid(row=5, column=1)
+
+        self._load_button = tk_Button(master=self, text='Load')
+        self._load_button.grid(row=6, column=1)
+        self._load_button.configure(command=self._load_button_command)
+
+    def _load_button_command(self):
+        if self._load_filename_entry.get() != '':
+            with open(self._load_filename_entry.get(), 'rb') as file_handle:
+                self._current_game_map.set(pickle_load(file_handle))
+
+                self._game_gui.render()
 
 
 class MapEditorException(ApplicationException):
