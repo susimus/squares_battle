fec2a0686f43a1ee5109c3a6bef3345ef6b12a4c
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-11-08 18:54:23 +0500

    Add raw maps loading from raw map name
    * Add launcher exception class

--- user_interface/launcher_ui.py
+++ user_interface/launcher_ui.py
@@ -1,14 +1,15 @@
-from argparse import ArgumentParser
-from typing import TextIO
+from argparse import ArgumentParser, Namespace
+from typing import TextIO, Optional
 from sys import exit as sys_exit
 
+from engine import ApplicationException
 from engine.engine import GameEngine
 from maps import RawMapsContainer
 
 
 def exit_with_exception(
         info_for_user: str,
-        input_exception: Exception,
+        input_exception: ApplicationException,
         debugging_mode: bool):
     """Exits with exception
 
@@ -65,8 +66,40 @@ def run_launcher_logic():
         action='version',
         version=get_current_version())
 
-    parser.parse_args()
+    parser.add_argument(
+        '-d', '--debug',
+        help="turn on debug info printing",
+        action='store_true')
 
-    # TODO: make GUI version of launcher. For now launcher just loads some map
-    game_engine = GameEngine(RawMapsContainer.get_map_2())
+    arguments: Namespace = parser.parse_args()
+
+    # WouldBeBetter: make GUI version of launcher. For now launcher just loads
+    #  some map
+
+    map_name: str = input(
+        'Enter map name. Raw maps names have format: "raw <name>".\n')
+
+    game_engine: Optional[GameEngine] = None
+
+    if map_name.startswith('raw '):
+        try:
+            game_engine = GameEngine(
+                getattr(
+                    RawMapsContainer,
+                    'get_map_' + map_name.split(' ')[1])())
+        except AttributeError:
+            exit_with_exception(
+                'Wrong raw map name',
+                LauncherException('Wrong raw map name: ' + map_name),
+                arguments.debug)
+
+    # TODO: Finish custom maps loading from 'maps' folder
+
+    if game_engine is None:
+        pass
+
     game_engine.start_game()
+
+
+class LauncherException(ApplicationException):
+    pass
