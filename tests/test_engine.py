2e876d6c6b950e8d7a180a76472958b9fe5fec2e
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-11-12 14:49:49 +0500

    Update tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -9,25 +9,27 @@ from sys import path as sys_path
 sys_path.append(os_path_join(
     os_path_dirname(os_path_abspath(__file__)), os_pardir))
 
+from engine.game_objects import Vector2D
 from engine.engine import GameEngine
-from maps.maps_processor import GameMap
+from maps import GameMap
 
 
 class VelocityGettersTests(TestCase):
     _state_updater: GameEngine.MapUpdater.StateUpdater = (
-        GameEngine.MapUpdater.StateUpdater(GameMap(), set()))
+        GameEngine.MapUpdater.StateUpdater(
+            GameEngine(GameMap(Vector2D(100, 100), [], []))))
 
     def test_velocity_to_the_left(self):
-        assert (
+        self.assertEqual(
+            -self._state_updater._PLAYER_MOVE_SPEED,
             self._state_updater._get_horizontal_velocity(
-                {self._state_updater._KEY_CODE_A})
-            == -self._state_updater._PLAYER_MOVE_SPEED)
+                {self._state_updater._KEY_CODE_A}))
 
     def test_velocity_to_the_right(self):
-        assert (
+        self.assertEqual(
+            self._state_updater._PLAYER_MOVE_SPEED,
             self._state_updater._get_horizontal_velocity(
-                {self._state_updater._KEY_CODE_D})
-            == self._state_updater._PLAYER_MOVE_SPEED)
+                {self._state_updater._KEY_CODE_D}))
 
     def test_no_horizontal_velocity_with_both_keys_pressed(self):
         assert (self._state_updater._get_horizontal_velocity(
@@ -38,7 +40,7 @@ class VelocityGettersTests(TestCase):
         assert self._state_updater._get_horizontal_velocity(set()) == 0
 
     def test_initial_jump_velocity(self):
-        self._state_updater._jump_is_available = True
+        self._state_updater._player_is_on_the_ground = True
 
         assert self._state_updater._get_vertical_velocity(
             {self._state_updater._KEY_CODE_SPACE}) == (
