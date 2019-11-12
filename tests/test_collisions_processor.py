7fc3fab50dbae6e61df7848ce6db64ff4a6932bd
Семен Вяцков <43330319+susimus@users.noreply.github.com>
2019-11-12 17:40:19 +0500

    Add test for player/basic platform collision

--- tests/test_collisions_processor.py
+++ tests/test_collisions_processor.py
@@ -11,87 +11,129 @@ sys_path.append(os_path_join(
     os_path_dirname(os_path_abspath(__file__)), os_pardir))
 
 from engine.collisions_processor import (
-    CollisionsProcessor, Collision, GameEvent)
-from maps.maps_processor import GameMap
-from engine.game_objects import Vector2D, Player, GameObject
+    CollisionsProcessor, Collision, GameEvent, CollisionsProcessorException)
+from maps import GameMap
+from engine.game_objects import (
+    Vector2D, Player, MovableObject, SpeedUpBuff, BasicPlatform)
 
 
-only_player_small_map: GameMap = GameMap(Vector2D(100, 100), Player())
+test_map_1: GameMap = GameMap(
+    Vector2D(100, 100),
+    [],
+    [Player(Vector2D(0, 0))])
+
+test_map_2: GameMap = GameMap(
+    Vector2D(100, 100),
+    [SpeedUpBuff(Vector2D(50, 50))],
+    [Player(Vector2D(0, 0))])
+
+test_map_3: GameMap = GameMap(
+    Vector2D(100, 100),
+    [BasicPlatform(Vector2D(30, 30), 10, 10)],
+    [Player(Vector2D(0, 0))])
 
 
 class ExceptionsTests(TestCase):
     def test_unknown_moving_object_exception(self):
-        with self.assertRaises(ValueError) as occurred_exc:
-            CollisionsProcessor(only_player_small_map).get_collisions(
-                GameObject(), Vector2D())
+        with self.assertRaises(CollisionsProcessorException) as occurred_exc:
+            CollisionsProcessor(test_map_1).get_collisions(
+                MovableObject(Vector2D(0, 0)), Vector2D(0, 0))
 
         assert len(occurred_exc.exception.args) == 1
         assert occurred_exc.exception.args[0] == (
-            'Got unknown class of [moving_object]: GameObject')
+            'While processing [get_collisions] method, got [moving_object] '
+            'with unknown type: MovableObject')
 
 
 class PlayerCollisionsTests(TestCase):
-    _collisions_processor: CollisionsProcessor = CollisionsProcessor(
-        only_player_small_map)
-
     def test_right_border_collision(self):
         right_border_collision: List[Collision] = (
-            self._collisions_processor.get_collisions(
-                only_player_small_map.player, Vector2D(200, 0)))
+            CollisionsProcessor(test_map_1).get_collisions(
+                test_map_1.movable_objects[0],
+                Vector2D(200, 0)))
 
-        assert len(right_border_collision) == 1
+        self.assertEqual(1, len(right_border_collision))
 
         assert (right_border_collision[0].game_event
-                is GameEvent.PLAYER_IS_OUT_RIGHT)
+                is GameEvent.PLAYER_BORDERS_RIGHT)
         assert (right_border_collision[0].moving_object
-                is only_player_small_map.player)
+                is test_map_1.movable_objects[0])
         assert right_border_collision[0].collided_object is None
 
     def test_left_border_collision(self):
         left_border_collision: List[Collision] = (
-            self._collisions_processor.get_collisions(
-                only_player_small_map.player, Vector2D(-200, 0)))
+            CollisionsProcessor(test_map_1).get_collisions(
+                test_map_1.movable_objects[0], Vector2D(-200, 0)))
 
-        assert len(left_border_collision) == 1
+        self.assertEqual(1, len(left_border_collision))
 
         assert (left_border_collision[0].game_event
-                is GameEvent.PLAYER_IS_OUT_LEFT)
+                is GameEvent.PLAYER_BORDERS_LEFT)
         assert (left_border_collision[0].moving_object
-                is only_player_small_map.player)
+                is test_map_1.movable_objects[0])
         assert left_border_collision[0].collided_object is None
 
     def test_top_border_collision(self):
         top_border_collision: List[Collision] = (
-            self._collisions_processor.get_collisions(
-                only_player_small_map.player, Vector2D(0, -200)))
+            CollisionsProcessor(test_map_1).get_collisions(
+                test_map_1.movable_objects[0], Vector2D(0, -200)))
 
-        assert len(top_border_collision) == 1
+        self.assertEqual(1, len(top_border_collision))
 
         assert (top_border_collision[0].game_event
-                is GameEvent.PLAYER_IS_OUT_TOP)
+                is GameEvent.PLAYER_BORDERS_TOP)
         assert (top_border_collision[0].moving_object
-                is only_player_small_map.player)
+                is test_map_1.movable_objects[0])
         assert top_border_collision[0].collided_object is None
 
     def test_bottom_border_collision(self):
         bottom_border_collision: List[Collision] = (
-            self._collisions_processor.get_collisions(
-                only_player_small_map.player, Vector2D(0, 200)))
+            CollisionsProcessor(test_map_1).get_collisions(
+                test_map_1.movable_objects[0], Vector2D(0, 200)))
 
-        assert len(bottom_border_collision) == 1
+        self.assertEqual(1, len(bottom_border_collision))
 
         assert (bottom_border_collision[0].game_event
-                is GameEvent.PLAYER_IS_OUT_BOTTOM)
+                is GameEvent.PLAYER_BORDERS_BOTTOM)
         assert (bottom_border_collision[0].moving_object
-                is only_player_small_map.player)
+                is test_map_1.movable_objects[0])
         assert bottom_border_collision[0].collided_object is None
 
     def test_no_borders_collisions(self):
         no_border_collision: List[Collision] = (
-            self._collisions_processor.get_collisions(
-                only_player_small_map.player, Vector2D(0, 10)))
+            CollisionsProcessor(test_map_1).get_collisions(
+                test_map_1.movable_objects[0], Vector2D(0, 10)))
 
-        assert len(no_border_collision) == 0
+        self.assertEqual(0, len(no_border_collision))
+
+    def test_player_buff_collision(self):
+        collisions: List[Collision] = (
+            CollisionsProcessor(test_map_2).get_collisions(
+                test_map_2.movable_objects[0],
+                Vector2D(30, 30)))
+
+        self.assertEqual(1, len(collisions))
+        self.assertEqual(GameEvent.PLAYER_BUFF, collisions[0].game_event)
+        self.assertEqual(
+            test_map_2.immovable_objects[0],
+            collisions[0].collided_object)
+        self.assertEqual(
+            test_map_2.movable_objects[0],
+            collisions[0].moving_object)
+
+    def test_player_basic_platform_collision(self):
+        collisions: List[Collision] = (
+            CollisionsProcessor(test_map_3).get_collisions(
+                test_map_3.movable_objects[0],
+                Vector2D(20, 20)))
+
+        self.assertEqual(1, len(collisions))
+        self.assertEqual(
+            GameEvent.PLAYER_TOP_BASIC_PLATFORM, collisions[0].game_event)
+        self.assertEqual(
+            test_map_3.movable_objects[0], collisions[0].moving_object)
+        self.assertEqual(
+            test_map_3.immovable_objects[0], collisions[0].collided_object)
 
 
 if __name__ == '__main__':
